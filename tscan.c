51834bb57c38f5d5584754d8b446a73d9d0d358d
Terry Bolton-Williams <55043494+TerrehDactyl@users.noreply.github.com>
2020-11-13 01:43:53 -0700

    Update tscan.c
    implemented gtktemplate library and renamed variables for better readability

--- tscan.c
+++ tscan.c
@@ -1,35 +1,19 @@
+#include "gtktemplate.h"
 #include <netinet/tcp.h>
-#include <gtk/gtk.h>
 #include <arpa/inet.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #define MAX_INT 32767
-void testconnection(char *input1, int port);
-void portscan(void);
-void createsinglesizegrid(GtkWidget *grid, gchar *labels[], void *callback[], int rows, int columns, const gchar *style[], int stylenumber);
-void add_context(const gchar *style, GtkWidget *widget);
-void set_spacing(GtkWidget *widget, int colspace, int rowspace);
-void button_connect_callback(GtkWidget *button, void *button_callback);
-void createdisplays(GtkWidget *widget, GtkWidget *buffer, const gchar *style[], gchar *startingtext[]);
-void testcallback(void);
-static void startportcallback( GtkWidget *widget, GtkWidget *entry);
-static void endportcallback( GtkWidget *widget, GtkWidget *entry);
-static void ipaddresscallback( GtkWidget *widget, GtkWidget *entry);
+void portscan();
 void startscan();
 void cancelscan();
 void savetofile();
 
-// compile with gcc -Wall -g portscan.c -o portscan `pkg-config --cflags --libs gtk+-3.0`
+// compile with gcc -Wall -g tscan.c -o tscan `pkg-config --cflags --libs gtk+-3.0`
 struct Global_Widgets
 {
-GtkWidget *outputdisplay; 
-GtkTextBuffer *outputbuffer; //widget for the text
-GtkWidget *startportentry;
-GtkWidget *endportentry;
-GtkWidget *ipaddressentry;
-int savecount;
+GtkWidget *entries[3];
 }gwidget;
 
 struct input_variables
@@ -37,6 +21,9 @@ struct input_variables
 const char *startport;
 const char *endport;
 const char *ipaddress;
+const char *entrytext[3];
+size_t label_len;
+int savecount;
 }input;
 
 int main(int argc, char *argv[])
@@ -45,106 +32,42 @@ GtkWidget *window; //widget for the window
 GtkWidget *vbox; //widget for the vertical box
 GtkWidget *hbox;
 GtkWidget *buttonbox;
-GtkWidget *buttonhbox;
-GtkWidget *label;
-GtkWidget *grid;
+GtkWidget *label_grid;
 GtkWidget *entrygrid;
 gchar *buttonlabels[] ={"Start Scan", "Cancel Scan", "Save To File"};
 void *buttoncallbacks [] = {startscan, cancelscan, savetofile};
-const gchar *style [] = {"window_style", "display_style", "button_style"};
-gchar *startingtext[] = {"Starting Port\n", "Ending Port\n", "IP Address"};
+gchar *labels[] = {"Starting Port\n", "Ending Port\n", "IP Address"};
+input.label_len = arraysize(labels);
 
 gtk_init(&argc, &argv); //starting gtk 
 
-window = gtk_window_new(GTK_WINDOW_TOPLEVEL); //creates toplevel window
-gtk_window_set_title(GTK_WINDOW(window), "TScan"); //sets a window title 
-gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); //opens the window in the center of the screen
-gtk_container_set_border_width(GTK_CONTAINER(window), 5); //sets the border size of the window
-add_context(style[0], window);
-
-grid = gtk_grid_new();
-
-for (int i=0; i<=2; i++)
-{
-	for(int j = 0; j<1; j++)
-	{	
-		label = gtk_label_new(startingtext[i]);
-		gtk_grid_attach(GTK_GRID(grid), label, j, i, 1, 1); //sets the defaults for creating each table button
-	}
-}
-
+window = createwindow("Tscan", GTK_WIN_POS_CENTER);
+label_grid = createlabels(labels, input.label_len);
 entrygrid = gtk_grid_new();
-gwidget.startportentry = gtk_entry_new ();
-gwidget.endportentry = gtk_entry_new();
-gwidget.ipaddressentry = gtk_entry_new();
-
-gtk_grid_attach(GTK_GRID(entrygrid), gwidget.startportentry, 0, 0, 1, 1); //sets the defaults for creating each table button
-gtk_grid_attach(GTK_GRID(entrygrid), gwidget.endportentry, 0,1,1,1);
-gtk_grid_attach(GTK_GRID(entrygrid), gwidget.ipaddressentry, 0,2,1,1);
-
- g_signal_connect (gwidget.startportentry, "activate", G_CALLBACK (startportcallback), gwidget.startportentry);
- g_signal_connect (gwidget.endportentry, "activate", G_CALLBACK (endportcallback), gwidget.endportentry);
- g_signal_connect (gwidget.ipaddressentry, "activate", G_CALLBACK (ipaddresscallback), gwidget.ipaddressentry);
-
-gwidget.outputdisplay = gtk_text_view_new (); //sets the display widget as a text display 
-gwidget.outputbuffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (gwidget.outputdisplay)); //sets the gwidget.buffer widget to the text gwidget.buffer of display
-gtk_text_buffer_set_text (gwidget.outputbuffer, "output", -1); //displays input.num1
-add_context(style[1], gwidget.outputdisplay);
-
-//g_signal_connect(window, "key-release-event", G_CALLBACK(keyboard_callback), NULL);
-
-buttonbox = gtk_grid_new(); 
-createsinglesizegrid(buttonbox, buttonlabels, buttoncallbacks, 1, 3, style, 2);
-set_spacing(buttonbox, 3, 3);
+create_entries(input.label_len, gwidget.entries, entrygrid);
+buttonbox = createsinglesizegrid(buttonlabels, buttoncallbacks, NULL, 1, 3);
 
 vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1); //creates a vbox without autosizing 
 hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1); //creates a vbox without autosizing 
-buttonhbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1); //creates a vbox without autosizing 
 
 gtk_container_add(GTK_CONTAINER(window), vbox); //adds the vbox to the window 
 gtk_box_pack_start(GTK_BOX(vbox),  hbox, FALSE, FALSE, 0); //packs the display into the vbox
-gtk_box_pack_start(GTK_BOX(hbox),  grid, FALSE, FALSE, 0); 
+gtk_box_pack_start(GTK_BOX(hbox),  label_grid, FALSE, FALSE, 0); 
 gtk_box_pack_start(GTK_BOX(hbox),  entrygrid, FALSE, FALSE, 0); 
-gtk_box_pack_start(GTK_BOX(vbox),  buttonhbox, FALSE, FALSE, 0); //packs the display into the vbox
-gtk_box_pack_start(GTK_BOX(buttonhbox),  buttonbox, FALSE, FALSE, 0); //packs the display into the vbox
-gtk_box_pack_start(GTK_BOX(vbox),  gwidget.outputdisplay, FALSE, FALSE, 0); //packs the display into the vbox
+gtk_box_pack_start(GTK_BOX(vbox),  buttonbox, FALSE, FALSE, 0); //packs the display into the vbox2
 
-g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-gtk_widget_show_all(window); //shows all widgets 
-
-gtk_main();//gtk main, this is the main loop of GTK
-}
-
-static void startportcallback( GtkWidget *widget, GtkWidget *entry)
-{
-  input.startport = gtk_entry_get_text (GTK_ENTRY (entry));
-  printf ("Starting Port: %s\n", input.startport);
-}
-
-static void endportcallback( GtkWidget *widget, GtkWidget *entry)
-{
-  input.endport = gtk_entry_get_text (GTK_ENTRY (entry));
-  printf ("Ending Port: %s\n", input.endport);
-}
-
-static void ipaddresscallback( GtkWidget *widget, GtkWidget *entry)
-{
-  input.ipaddress = gtk_entry_get_text (GTK_ENTRY (entry));
-  printf ("IP Address: %s\n", input.ipaddress);
+show_and_destroy(window);
 }
 
 void startscan()
 {
 	struct sockaddr_in server_address;
-	startportcallback(gwidget.startportentry, gwidget.startportentry);
-	endportcallback(gwidget.endportentry, gwidget.endportentry);
-	ipaddresscallback(gwidget.ipaddressentry, gwidget.ipaddressentry);
-	int start = atoi(input.startport);
-	int end = atoi(input.endport);
+	get_entry_text(gwidget.entries, input.entrytext, input.label_len);
+	int start = atoi(input.entrytext[0]);
+	int end = atoi(input.entrytext[1]);
 	FILE *filepointer;
 
-	if (gwidget.savecount > 0)
+	if (input.savecount > 0)
 	{
 		filepointer = fopen("Results.txt", "w");
 	}
@@ -163,33 +86,36 @@ for(int i = start; i<=end; i++)
         printf("Socket successfully created..\n"); 
     }
     char hostname[MAX_INT];
-    strncpy(hostname, input.ipaddress, 15);
-	server_address.sin_addr.s_addr = inet_addr(input.ipaddress);
+    strncpy(hostname, input.entrytext[2], 15);
+	server_address.sin_addr.s_addr = inet_addr(input.entrytext[2]);
 	int err = 0;
 	err = connect(network_socket,(struct sockaddr *) &server_address,sizeof server_address);
 	if (err <0)
 	{
-		gtk_text_buffer_set_text (gwidget.outputbuffer, "closed", -1); //displays input.num1
-		printf("Port: %d is closed\n", i);
+		g_print("Port: %d is closed\n", i);
 
-		if (gwidget.savecount > 0)
+		if (input.savecount > 0)
 		{
 			fprintf(filepointer, "Port: %d is closed\n", i);
 		}
 	}
 	else
 	{
-		gtk_text_buffer_set_text (gwidget.outputbuffer, "open", -1); //displays input.num1
-		printf("Port: %d is open\n", i);
+		g_print("Port: %d is open\n", i);
 
-		if (gwidget.savecount > 0)
+		if (input.savecount > 0)
 		{
 			fprintf(filepointer, "Port: %d is open\n", i);
 		}
 	}
 	close(network_socket);
 }
-fclose(filepointer);
+g_print("program finished");
+
+if (input.savecount > 0)
+		{
+			fclose(filepointer);
+		}
 }
 
 void cancelscan()
@@ -199,43 +125,5 @@ void cancelscan()
 
 void savetofile()
 {
-	gwidget.savecount = 1;
-}
-
-void set_spacing(GtkWidget *widget, int colspace, int rowspace)
-{
-  gtk_grid_set_column_spacing(GTK_GRID(widget), colspace);
-  gtk_grid_set_row_spacing(GTK_GRID(widget), rowspace);
-}
-
-void createsinglesizegrid(GtkWidget *grid, gchar *labels[], void *callback[], int rows, int columns, const gchar *style[], int stylenumber)
-{
- int pos = 0;
-GtkWidget *button; //widget for the buttons 
-for (int i=0; i < rows; i++) //for loop for the rows
-{
-for (int j=0; j < columns; j++) //for loop for the columns
-{
-button = gtk_button_new_with_label(labels[pos]); //sets each button label to the respective button 
-button_connect_callback(button, callback[pos]); //attaches the button to the respective callback
-gtk_grid_attach(GTK_GRID(grid), button, j, i, 1, 1); //sets the defaults for creating each table button
-gtk_widget_set_size_request(button, 40, 40); //sets the size of the buttons
-add_context(style[stylenumber], button);
-pos++; //changes the position 
-}
-}
-}
-
-void add_context(const gchar *style, GtkWidget *widget)
-{
-  GtkCssProvider* Provider = gtk_css_provider_new();
-  gtk_css_provider_load_from_path(GTK_CSS_PROVIDER(Provider), "Styles.css", NULL);
-  GtkStyleContext *context = gtk_widget_get_style_context(widget);
-  gtk_style_context_add_class(context, style);
-  gtk_style_context_add_provider (context,GTK_STYLE_PROVIDER(Provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
-}
-
-void button_connect_callback(GtkWidget *button, void *button_callback)
-{
-  g_signal_connect(button, "clicked", G_CALLBACK(button_callback), NULL);
+	input.savecount = 1;
 }
